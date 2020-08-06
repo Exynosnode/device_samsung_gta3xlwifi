0e780bed091e225018a01ca34d23a2503c0cd144
midhunvnadh <midhun.vnadh5732@gmail.com>
2020-08-06 14:57:56 +0530

    [Halium]: Initial Commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,4 +9,81 @@ DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := gta3xlwifi
+
+# Include
+TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := exynos5
+TARGET_SOC := exynos7885
+TARGET_BOARD_PLATFORM_GPU := mali-g71
+TARGET_BOOTLOADER_BOARD_NAME := universal7885
+TARGET_USES_UEFI := true
+TARGET_NO_RADIOIMAGE := true
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+
+# Secondary Architecture
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Extracted with libbootimg
+BOARD_CUSTOM_BOOTIMG := true
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+BOARD_KERNEL_IMAGE_NAME := Image
+#BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := console=tty0 androidboot.selinux=permissive
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPRL04A004RU
+#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+
+# Kernel
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CONFIG := halium-gta3xlwifi_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/gta3xlwifi
+#TARGET_KERNEL_CLANG_COMPILE := false
+
+# Partitions
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x02000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x02D00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0xB8000000
+
+# Workaround for error copying vendor files to recovery ramdisk
+TARGET_COPY_OUT_VENDOR := vendor
+
+# Use mke2fs to create ext4 images
+TARGET_USES_MKE2FS := true
+
+#Adds
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+
+# Extended Filesystem Support
+TARGET_EXFAT_DRIVER := sdfat
+
+# Properties
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
+# Android Verified Boot
+BOARD_AVB_ENABLE := false
+BOARD_BUILD_DISABLED_VBMETAIMAGE := true
+
 -include vendor/samsung/gta3xlwifi/BoardConfigVendor.mk
--- lineage_gta3xlwifi.mk
+++ lineage_gta3xlwifi.mk
@@ -1,12 +1,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from gta3xlwifi device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+# Inherit halium source
+$(call inherit-product, $(SRC_TARGET_DIR)/product/halium.mk)
 
 PRODUCT_BRAND := samsung
 PRODUCT_DEVICE := gta3xlwifi
