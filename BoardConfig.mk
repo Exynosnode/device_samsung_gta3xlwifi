8683c00a42d9002dbeed0abb3c84eeedef610bf7
midhunvnadh <midhun.vnadh5732@gmail.com>
2020-08-09 22:58:56 +0530

    Update Board SKU

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,13 +40,13 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 
 # Extracted with libbootimg
 BOARD_CUSTOM_BOOTIMG := true
-BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+#BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 #BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE := console=tty0 androidboot.selinux=permissive
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPRL04A004RU
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --header_version 1 --board SRPSA25A003KU
 #TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 
 # Kernel
